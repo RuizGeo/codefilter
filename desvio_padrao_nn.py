21fd0763a5817d1b939ca415122fb52b4603fe04
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-03-29 19:04:09 -0300

    Update desvio_padrao_nn.py

--- desvio_padrao_nn.py
+++ desvio_padrao_nn.py
@@ -1,8 +1,6 @@
-from osgeo import ogr
+# -*- coding: utf-8 -*-
 from PyQt4.QtCore import *
 import time
-import sys
-
 #-------PostGIS--------#
 import psycopg2
 try:
@@ -15,26 +13,13 @@ cursor = connection.cursor()
 layer = iface.activeLayer()
 provider = layer.dataProvider()
 spIndex = QgsSpatialIndex() #create spatial index object
-fit = provider.getFeatures() #gets all features in layer
-# insert features to index
-feat = QgsFeature()
-while fit.nextFeature(feat):
-    spIndex.insertFeature(feat)
 #Percorrer as geometrias
 iter = layer.getFeatures()
-iniGeom = time.time()
+iniIter = time.time()
 for feature in iter:
-    #Obter propriedades da geometria
-    geom = feature.geometry()
-    #A partir do ponto obter os mais proximos, retorna o ID das colunas
-    nearestIds = spIndex.nearestNeighbor(geom.asPoint(),2)
-    #Retorna uma tupla dos IDs das linhas selecionadas
-    t = tuple([int(i) for i in nearestIds])
     #Calcula a media para os IDs selecionados
-    cursor.execute("SELECT AVG(z) FROM point_cloud WHERE gid in %s" % (t,))
-    #Transforma o valor em float
-    float([str(i).split("'")[1] for i in cursor][0])
- 
-print float([str(i).split("'")[1] for i in cursor][0])
-fimGeom = time.time()
-print 'Tempo Geom: ',(fimGeom - iniGeom) , ' segundos'
+    cursor.execute("SELECT tan(AVG(p1.z)/AVG(ST_Distance(p.geom, p1.geom))) FROM point_cloud AS p, point_cloud AS p1 WHERE ST_DWithin(p1.geom,p.geom, 2.0) AND ST_Distance(p.geom, p1.geom) != 0 AND p.gid = %s" %feature.id())
+    
+print [i for i in cursor]
+fimIter = time.time()
+print 'Tempo iter: ',(fimIter - iniIter) , ' segundos'
